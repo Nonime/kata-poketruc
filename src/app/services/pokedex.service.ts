272eafb732e92234a789a62f277098fc95add270
Romain Carlo <romain.carlo@pole-emploi.fr>
2023-06-23 14:55:00 +0200

    Imple pokedex service

--- src/app/services/pokedex.service.ts
+++ src/app/services/pokedex.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { environment } from 'src/environment/environment';
 import { PokemonResponse } from '../interfaces/PokemonResponse';
+import {constantes} from '../../constants';
 
 @Injectable({
   providedIn: 'root'
@@ -14,5 +15,11 @@ export class PokedexService {
   constructor(private http: HttpClient) { }
 
   getPokemonInfos(pokemonName: string) {
+    let url = constantes.URL_POKE_API + pokemonName;
+    console.log('url:' + url);
+    this.http.get<PokemonResponse>(url).subscribe((value)   => {
+      this.currentPokemon$.next(value);
+    });
+    return this.currentPokemon$;
   }
 }
